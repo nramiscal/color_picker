cabe40985d579dfb2d758b8dbf67b66199a29301
nramiscal <nramiscal@gmail.com>
2017-10-06 17:30:02 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, redirect, render_template, session
+app = Flask(__name__)
+
+app.secret_key = "hideme"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=["POST"])
+def process():
+    red = request.form['red']
+    green = request.form['green']
+    blue = request.form['blue']
+    print red
+    print green
+    print blue
+    return render_template('index.html', red=red, green=green, blue=blue)
+
+app.run(debug=True)
